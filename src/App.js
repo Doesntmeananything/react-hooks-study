c1210449186571316cdd3e6f98c2348a3748fd53
Doesntmeananything <andrey.krasnov.pm@gmail.com>
2019-05-28 16:04:28 +0300

    Add simple useEffect hook

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Toggle from "./Toggle";
 
 const App = () => {
   const [name, setName] = useState("");
 
+  useEffect(() => {
+    document.title = name;
+  });
+
   return (
     <div className="main-wrapper">
       <h1>Level Up Dishes</h1>
       <Toggle />
-      <h3>{name}</h3>
-      <input type="text" onChange={e => setName(e.target.value)} value={name} />
+      <form
+        onSubmit={e => {
+          e.preventDefault();
+        }}
+      >
+        <input
+          type="text"
+          onChange={e => setName(e.target.value)}
+          value={name}
+        />
+        <button>Submit</button>
+      </form>
     </div>
   );
 };
