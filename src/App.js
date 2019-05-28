3a154df0023c6b207f15de9e559f1601bbeee950
Doesntmeananything <andrey.krasnov.pm@gmail.com>
2019-05-28 18:51:34 +0300

    Add data fetching with useEffect

--- src/App.js
+++ src/App.js
@@ -1,46 +1,56 @@
-import React, { useRef, useMemo, createContext } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import Toggle from "./Toggle";
 import useTitleInput from "./hooks/useTitleInput";
-
-export const UserContext = createContext();
 
 const App = () => {
   const [name, setName] = useTitleInput("");
   const ref = useRef();
+  const [dishes, setDishes] = useState([]);
 
-  const reverseWord = word => {
-    return word
-      .split("")
-      .reverse()
-      .join("");
+  const fetchDishes = async () => {
+    const res = await fetch(
+      "https://my-json-server.typicode.com/leveluptuts/fakeapi/dishes"
+    );
+    const data = await res.json();
+    setDishes(data);
   };
 
-  const title = "Level Up Dishes";
-  const TitleReversed = useMemo(() => reverseWord(title), [title]);
+  useEffect(() => {
+    fetchDishes();
+  }, []);
 
   return (
-    <UserContext.Provider value={{ user: true }}>
-      <div
-        className="main-wrapper"
-        ref={ref}
-        onClick={() => ref.current.classList.add("clicked")}
+    <div
+      className="main-wrapper"
+      ref={ref}
+      onClick={() => ref.current.classList.add("clicked")}
+    >
+      <h1>Level Up Dishes</h1>
+      <Toggle />
+      <form
+        onSubmit={e => {
+          e.preventDefault();
+        }}
       >
-        <h1>{TitleReversed}</h1>
-        <Toggle />
-        <form
-          onSubmit={e => {
-            e.preventDefault();
-          }}
-        >
-          <input
-            type="text"
-            onChange={e => setName(e.target.value)}
-            value={name}
-          />
-          <button>Submit</button>
-        </form>
-      </div>
-    </UserContext.Provider>
+        <input
+          type="text"
+          onChange={e => setName(e.target.value)}
+          value={name}
+        />
+        <button>Submit</button>
+      </form>
+      {dishes.map(dish => (
+        <article key={dish.name} className="dish-card dish-card--withImage">
+          <h3>{dish.name}</h3>
+          <p>{dish.desc}</p>
+          <div className="ingredients">
+            {dish.ingredients.map(ingredient => (
+              <span key={ingredient}>{ingredient}</span>
+            ))}
+          </div>
+        </article>
+      ))}
+    </div>
   );
 };
 
--- src/hooks/useTitleInput.js
+++ src/hooks/useTitleInput.js
@@ -1,10 +1,12 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useDebugValue } from "react";
 
 function useTitleInput(initialValue) {
   const [value, setValue] = useState(initialValue);
   useEffect(() => {
     document.title = value;
   });
+  useDebugValue("Debugging value");
+
   return [value, setValue];
 }
 
