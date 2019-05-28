153a46a5db0e4c2d8ae5394a5c4e3232e8bfe346
Doesntmeananything <andrey.krasnov.pm@gmail.com>
2019-05-28 20:37:46 +0300

    Add animation to title fade-in

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState, useRef } from "react";
 import useAbortableFetch from "use-abortable-fetch";
+import { useSpring, animated } from "react-spring";
 import Toggle from "./Toggle";
 import useTitleInput from "./hooks/useTitleInput";
 
@@ -9,7 +10,8 @@ const App = () => {
   const { data, loading } = useAbortableFetch(
     "https://my-json-server.typicode.com/leveluptuts/fakeapi/dishes"
   );
-  if (!data) return null;
+
+  const props = useSpring({ opacity: 1, from: { opacity: 0 } });
 
   return (
     <div
@@ -17,7 +19,7 @@ const App = () => {
       ref={ref}
       onClick={() => ref.current.classList.add("clicked")}
     >
-      <h1>Level Up Dishes</h1>
+      <animated.h1 style={props}>Level Up Dishes</animated.h1>
       <Toggle />
       <form
         onSubmit={e => {
@@ -31,17 +33,18 @@ const App = () => {
         />
         <button>Submit</button>
       </form>
-      {data.map(dish => (
-        <article key={dish.name} className="dish-card dish-card--withImage">
-          <h3>{dish.name}</h3>
-          <p>{dish.desc}</p>
-          <div className="ingredients">
-            {dish.ingredients.map(ingredient => (
-              <span key={ingredient}>{ingredient}</span>
-            ))}
-          </div>
-        </article>
-      ))}
+      {data &&
+        data.map(dish => (
+          <article key={dish.name} className="dish-card dish-card--withImage">
+            <h3>{dish.name}</h3>
+            <p>{dish.desc}</p>
+            <div className="ingredients">
+              {dish.ingredients.map(ingredient => (
+                <span key={ingredient}>{ingredient}</span>
+              ))}
+            </div>
+          </article>
+        ))}
     </div>
   );
 };
