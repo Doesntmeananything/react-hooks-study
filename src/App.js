84d783f6b651d0592212b27cac74f0abdc1a57ad
Doesntmeananything <andrey.krasnov.pm@gmail.com>
2019-05-28 17:50:35 +0300

    Add counter with useReducer hook

--- src/App.js
+++ src/App.js
@@ -1,27 +1,38 @@
-import React from "react";
+import React, { useRef, createContext } from "react";
 import Toggle from "./Toggle";
+import Counter from "./Counter";
 import useTitleInput from "./hooks/useTitleInput";
+
+export const UserContext = createContext();
 
 const App = () => {
   const [name, setName] = useTitleInput("");
+  const ref = useRef();
 
   return (
-    <div className="main-wrapper">
-      <h1>Level Up Dishes</h1>
-      <Toggle />
-      <form
-        onSubmit={e => {
-          e.preventDefault();
-        }}
+    <UserContext.Provider value={{ user: true }}>
+      <div
+        className="main-wrapper"
+        ref={ref}
+        onClick={() => ref.current.classList.add("clicked")}
       >
-        <input
-          type="text"
-          onChange={e => setName(e.target.value)}
-          value={name}
-        />
-        <button>Submit</button>
-      </form>
-    </div>
+        <h1>Level Up Dishes</h1>
+        <Toggle />
+        <Counter />
+        <form
+          onSubmit={e => {
+            e.preventDefault();
+          }}
+        >
+          <input
+            type="text"
+            onChange={e => setName(e.target.value)}
+            value={name}
+          />
+          <button>Submit</button>
+        </form>
+      </div>
+    </UserContext.Provider>
   );
 };
 
